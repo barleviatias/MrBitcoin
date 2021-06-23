63dfb57f172c182e57cec4bba26ef8dec6debd63
barleviatias <barlevi.atias@gmail.com>
2021-06-23 11:29:43 +0300

    some name

---
+++ src/app/model/contact.model.ts
@@ -0,0 +1,26 @@
+export interface Contact {
+    _id?: string
+    name: string
+    email: string
+    phone: string
+    move: any
+    coins: number
+    imgUrl: string
+}
+
+
+
+
+
+// export class Contact {
+
+//     constructor(public _id?: string, public name: string = '', public email: string = '', public phone: string = '') {
+
+//     }
+
+//     setId?() {
+//         // Implement your own set Id
+//         this._id = 'hkjgbjhb '
+//     }
+// }
+
