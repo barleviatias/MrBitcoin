9a42630209a2f8bfbe1920ff8bee6bb3443fe8a7
barleviatias <barlevi.atias@gmail.com>
2021-06-23 11:54:38 +0300

    Auto-generated commit [ci skip]

--- src/app/model/filterBy.model.ts
+++
@@ -1,4 +0,0 @@
-export interface FilterBy {
-    term: string
-  }
-  
--- src/app/model/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    _id?: string
-    name: string
-    username: string
-    coins: number
-    moves: any
-    password: string
-    imgUrl?: string
-}
